a6bd03dacea566122192d5dbaf1d692c08f3c035
Juan Gómez <gomezj01@stepstone.com>
2022-08-04 11:13:30 -0600

    Use custom middlewares

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Use redux with react
 ## Redux
 ### Redux Devtools
 - [redux-devtools](https://github.com/reduxjs/redux-devtools/tree/main/extension#installation)
+
+## Others
+### Trird-party APIs
+- [PokeApi](https://pokeapi.co/)
+
+### Palette Colors
+- [colorhunt](https://colorhunt.co/palettes/dark)
+- [coolors](https://coolors.co/)
+
+### Ant Design
+- [Ant Design - React](https://ant.design/docs/react/introduce)
--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import { pokemonsReducer } from './reducers/pokemonsReducer';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux'
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux'
+import { featuring, logger } from './middlewares';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring)
+)
+
 const store = createStore(
   pokemonsReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composedEnhancers
 );
 
 root.render(
